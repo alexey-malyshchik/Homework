ad5765ff6d1459f2ac3f4766f8d496da2185f90a
Alexy Malyshchik <alexey.malyshchik@yandex.ru>
2019-11-21 14:57:57 +1000

    Task2 add

--- Week1/Homework_1/Task2.php
+++ Week1/Homework_1/Task2.php
@@ -1,6 +1,10 @@
 <?php
-$pictures = 80;
-$markers = 23;
-$pencils = 40;
-$paints = $pictures - ($markers + $pencils);
+CONST PICTURES = 80;
+CONST MARKERS = 23;
+CONST PENCILS = 40;
+echo "Дано: " . PICTURES . " рисунков.<br>";
+echo "Из них " . MARKERS . " выполнены фломастером,<br>";
+echo PENCILS . " карандашами.<br>";
+echo "Рисунков, выполненные красками, было: " . $paints = PICTURES -MARKERS - PENCILS;
+
 
