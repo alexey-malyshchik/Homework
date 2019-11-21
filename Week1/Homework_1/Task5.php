1c3a8705fe513cdb6af121be77c235a6f03c0a62
Alexy Malyshchik <alexey.malyshchik@yandex.ru>
2019-11-21 16:48:29 +1000

    Task 5 add

--- Week1/Homework_1/Task5.php
+++ Week1/Homework_1/Task5.php
@@ -17,7 +17,7 @@ echo ($cars[0]['model'] . ' ' . $cars[0]['speed'] . ' ' . $cars[0]['doors']) .
 ' ' . $cars[0]['year'];
 echo '<br>Car Opel<br>';
 echo ($cars[1]['model'] . ' ' . $cars[1]['speed'] . ' ' . $cars[1]['doors']) .
-    ' ' . $cars[1]['year'];
+' ' . $cars[1]['year'];
 echo '<br>Car Toyota<br>';
 echo ($cars[2]['model'] . ' ' . $cars[2]['speed'] . ' ' . $cars[2]['doors']) .
-    ' ' . $cars[2]['year'];
+' ' . $cars[2]['year'];
