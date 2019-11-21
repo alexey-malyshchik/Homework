b2b32d71b6bee790287ddfa41b819371a5dd2cdc
Alexy Malyshchik <alexey.malyshchik@yandex.ru>
2019-11-21 15:34:48 +1000

    Task2 and Task 3 add

--- Week1/Homework_1/Task2.php
+++ Week1/Homework_1/Task2.php
@@ -2,9 +2,8 @@
 CONST PICTURES = 80;
 CONST MARKERS = 23;
 CONST PENCILS = 40;
-echo "Дано: " . PICTURES . " рисунков.<br>";
-echo "Из них " . MARKERS . " выполнены фломастером,<br>";
-echo PENCILS . " карандашами.<br>";
-echo "Рисунков, выполненные красками, было: " . $paints = PICTURES -MARKERS - PENCILS;
+echo 'Дано: ' . PICTURES . ' рисунков.<br>';
+echo 'Из них ' . MARKERS . ' выполнены фломастером, ' . PENCILS . ' карандашами.<br>';
+echo 'Рисунков, выполненные красками, было: ' . $paints = PICTURES - MARKERS - PENCILS . '.';
 
 
