53250332e0243acfad0ea938aed6ff9a61612956
Alexy Malyshchik <alexey.malyshchik@yandex.ru>
2019-11-25 15:31:20 +1000

    homework 2 add

---
+++ Week1/Homework_2/functions.php
@@ -0,0 +1,58 @@
+<?php
+function task1(array $strings, bool $unite_strings = false)
+{
+    if ($unite_strings) {
+        foreach ($strings as $word) {
+            $string .= $word;
+        }
+        return $string;
+    } else {
+        foreach ($strings as $word) {
+            $string = '<p>' . $word . '<p>';
+            echo $string;
+        }
+    }
+}
+
+function task3(int $rows, int $cols)
+{
+    echo '<table border="1" cellspacing="0" cellpadding="4">';
+    for ($tr = 1; $tr <= $rows; $tr++) {
+        echo '<tr>';
+        for ($td = 1; $td <= $cols; $td++) {
+            echo '<td>' . $tr * $td . '</td>';
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
+}
+
+function task4()
+{
+    echo date('d.m.Y G:i') . '<br>';
+}
+
+function task5()
+{
+    echo mktime(0, 0, 0, 2, 24, 2016) . '<br>';
+}
+
+function task6($string, $letter)
+{
+    echo str_replace("$letter", '', $string) . '<br>';
+}
+
+function task7($string, $search, $replace)
+{
+    echo str_replace("$search", "$replace", $string) . '<br>';
+}
+
+function task8()
+{
+    file_put_contents('test.txt', 'Hello again!');
+}
+
+function task9($file_name)
+{
+    echo file_get_contents($file_name);
+}
---
+++ Week1/Homework_2/index.php
@@ -0,0 +1,11 @@
+<?php
+require('functions.php');
+
+task1([stirng1, string2, string3, string4]);
+task3(8, 8);
+task4();
+task5();
+task6('Карл у Клары украл Кораллы', 'К');
+task7('Две бутылки лимонада', 'Две', 'Три');
+task8();
+task9('test.txt');
